787990ac8c5f76371998f4598cd324679b964515
Yohann BEAURE <ybeaure@student.42.fr>
2016-11-28 22:07:34 +0100

    checks update + zjmp + modif.h + Makefile

--- corewar/srcs/checks.c
+++ corewar/srcs/checks.c
@@ -6,11 +6,13 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 18:26:30 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/11/28 18:47:31 by ybeaure          ###   ########.fr       */
+/*   Updated: 2016/11/28 22:07:11 by ybeaure          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/corewar.h"
+
+extern t_op		op_tab[17];
 
 int		check_params(char opcode, char p_code[4], int p_val[4])
 {
@@ -21,7 +23,9 @@ int		check_params(char opcode, char p_code[4], int p_val[4])
 		return (0);
 	while (i < op_tab[(int)opcode - 1].nb_arg)
 	{
-		if ((op_tab[(int)opcode - 1].p_type[i] >> (p_code[i] - 1)) % 2 == 0)
-			return (0);
+//		if ((op_tab[(int)opcode - 1].p_type[i] >> (p_code[i] - 1)) % 2 == 0)
+//			return (0);
+//			f
 	}
+	return (1);
 }
--- corewar/srcs/zjmp.c
+++ corewar/srcs/zjmp.c
@@ -6,12 +6,16 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 17:23:44 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/11/28 18:47:31 by ybeaure          ###   ########.fr       */
+/*   Updated: 2016/11/28 21:18:32 by ybeaure          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/corewar.h"
 
+extern t_op		op_tab[17];
+
 void		ft_zjmp(t_vm *vm, t_process *pr, char p_code[4], int p_val[4])
 {
+	if (pr->carry == 0 && check_params(9, p_code, p_val))
+		pr->pc = (pr->pc + ((signed short)p_val[0] % IDX_MOD) + MEM_SIZE) % MEM_SIZE;
 }
