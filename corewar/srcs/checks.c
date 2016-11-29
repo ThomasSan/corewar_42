08af2310a53f9700b2a5a805996652d8b5472771
Yohann BEAURE <ybeaure@student.42.fr>
2016-11-29 07:33:17 +0100

    p_type check done / modif zjmp

--- corewar/srcs/checks.c
+++ corewar/srcs/checks.c
@@ -6,7 +6,7 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 18:26:30 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/11/28 22:07:11 by ybeaure          ###   ########.fr       */
+/*   Updated: 2016/11/29 07:32:47 by ybeaure          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,13 @@ int		check_params(char opcode, char p_code[4], int p_val[4])
 		return (0);
 	while (i < op_tab[(int)opcode - 1].nb_arg)
 	{
-//		if ((op_tab[(int)opcode - 1].p_type[i] >> (p_code[i] - 1)) % 2 == 0)
-//			return (0);
-//			f
+		// Toujours != 0 -- Si pb dans les types ca return 0
+		if ((op_tab[(int)opcode - 1].p_type[i] >> (p_code[i] - 1)) % 2 == 0)
+			return (0);
+		// Check si jamais il a le pcode mais la mauvaise value dans la mem
+		if (p_code[i] == REG_CODE && p_val[i] > 16)
+				return (0);
+			i++;
 	}
 	return (1);
 }
--- corewar/srcs/zjmp.c
+++ corewar/srcs/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 17:23:44 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/11/28 21:18:32 by ybeaure          ###   ########.fr       */
+/*   Updated: 2016/11/29 07:25:27 by ybeaure          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 extern t_op		op_tab[17];
 
-void		ft_zjmp(t_vm *vm, t_process *pr, char p_code[4], int p_val[4])
+void		ft_zjmp(t_vm *vm, t_process *pro, char p_code[4], int p_val[4])
 {
-	if (pr->carry == 0 && check_params(9, p_code, p_val))
-		pr->pc = (pr->pc + ((signed short)p_val[0] % IDX_MOD) + MEM_SIZE) % MEM_SIZE;
+	if (pro->carry == 0 && check_params(9, p_code, p_val))
+		pro->pc = (pro->pc + ((signed short)p_val[0] % IDX_MOD) + MEM_SIZE) % MEM_SIZE;
 }
