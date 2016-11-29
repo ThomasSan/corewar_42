2eb0c175c99235d53f1e624f28688ddbabafb3e4
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-11-29 15:53:38 +0100

    Mise a jour de la libft

--- corewar/libft/ft_bswap.c
+++ corewar/libft/ft_bswap.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 13:22:31 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/11/29 13:27:19 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/11/29 15:52:33 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ unsigned short		ft_bswap16(unsigned short value)
 
 unsigned int		ft_bswap32(unsigned int value)
 {
-	value = (value & 0x0000FFFF) << 16 | (value & 0xFFFF0000) >> 16;
-	return ((value & 0x00FF00FF) >> 8 | (value & 0xFF00FF00) << 8);
+	return (ft_bswap16(value & 0xFFFF) << 16 | (ft_bswap16(value >> 16)));
 }
