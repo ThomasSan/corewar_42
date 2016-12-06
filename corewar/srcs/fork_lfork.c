551631b28b4f582cd9c00714260d23e1869c0b1f
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-12-06 12:16:13 +0100

    debug fork_lfork.c

--- corewar/srcs/fork_lfork.c
+++ corewar/srcs/fork_lfork.c
@@ -6,7 +6,7 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 17:22:46 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/12/06 11:13:55 by ybeaure          ###   ########.fr       */
+/*   Updated: 2016/12/06 11:47:07 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 void		ft_fork(t_vm *vm, t_process *pro, char p_code[4], int p_val[4])
 {
+	int		pc;
+
 	if (check_params(12, p_code, p_val))
 	{
-	add_process(&vm->process, ((pro->pc + ((signed short)p_val[0] % IDX_MOD) + MEM_SIZE) % MEM_SIZE), pro->curr_op, pro->carry);
+		display_debug_ram(vm->ram);
+		pc = (pro->pc + ((signed short)p_val[0] % IDX_MOD) + MEM_SIZE) % MEM_SIZE;
+		add_process(&vm->process, pc, vm->ram[pc].value, pro->carry);
 	}
 }
 
