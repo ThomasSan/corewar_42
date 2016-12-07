4e1a83a1f5ef9ffb2a08a37fb364cb108bdcd0ca
Yohann BEAURE <ybeaure@student.42.fr>
2016-12-07 15:52:35 +0100

    lol

--- corewar/srcs/live.c
+++ corewar/srcs/live.c
@@ -6,7 +6,7 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 17:17:21 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/12/07 15:20:09 by ybeaure          ###   ########.fr       */
+/*   Updated: 2016/12/07 15:52:32 by ybeaure          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void		ft_live(t_vm *vm, t_process *pro, char p_code[4], int p_val[4])
 	{
 		vm->last_to_live = p_val[0] - 10;
 		pro->lives++;
-	ft_printf("%s%d%s%s\n", "Le joueur ", (p_val[0] - 10), " [", vm->champs[p_val[0] - 10].header.prog_name, "] est en vie");
+//	ft_printf("%s%d%s%s\n", "Le joueur ", (p_val[0] - 10), " [", vm->champs[p_val[0] - 10].header.prog_name, "] est en vie");
 //		ft_printf("%s%d%s%s%s\n", "Le joueur ", vm->last_to_live, " [", vm->champs[vm->last_to_live].header.prog_name, "] est en vie");
 	}
 }
