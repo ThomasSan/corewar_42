e0a2ebede6bcc32c88a42d5a51a9b25f3970c73c
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-12-09 18:35:14 +0100

    SA FONCTIONNE SA PUTAIN DE MERE

--- corewar/srcs/display_debug.c
+++ corewar/srcs/display_debug.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/30 12:16:39 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/12/09 17:17:19 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/12/09 18:27:06 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,8 @@ void			display_debug_ram(t_ram *ram, int cycle)
 	len = 1;
 	i = -1;
 	ft_printf("\n----- RAM %d -----\n\n", cycle);
-	//while (++i < MEM_SIZE)
-	while(++i < 100)
+	while (++i < MEM_SIZE)
+	//while(++i < 100)
 	{
 		if (ram[i].executed == 1)
 			ft_printf("\x1b[30m\x1b[47m%.2x\x1b[0m ", ram[i].value);
--- corewar/srcs/or_xor.c
+++ corewar/srcs/or_xor.c
@@ -6,7 +6,7 @@
 /*   By: ybeaure <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 17:19:38 by ybeaure           #+#    #+#             */
-/*   Updated: 2016/12/09 17:53:56 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/12/09 18:33:04 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,29 +49,29 @@ void		ft_xor(t_vm *vm, t_process *pro, char p_code[4], int p_val[4])
 		pro->reg[p_val[2] - 1] = get_new_p_val(vm, pro, p_code[0], p_val[0]) ^ get_new_p_val(vm, pro, p_code[1], p_val[1]);
 
 
-
+/*
 		if (get_new_p_val(vm, pro, p_code[0], p_val[0]) == 14 && i == 0)
 		{
 			i = 0;
 			vm->A_supprimer = pro;
 		}
-
+*/
 
 
 		if (pro->reg[p_val[2] - 1] == 0)
 		{
-			ft_printf("CARRY = 1\ncycle : %d\n", vm->cycle.cycles);
+//			ft_printf("CARRY = 1\ncycle : %d\n", vm->cycle.cycles);
 			pro->carry = 1;
 		}
 		else
 		{
-			if (pro == vm->A_supprimer)
+/*			if (pro == vm->A_supprimer)
 			{
 				ft_printf("CARRY = 0\ncycle : %d\nvalue REG : %d\n", vm->cycle.cycles, pro->reg[p_val[2] - 1]);
 				ft_printf("gnpv 1 : %#x\ngnpv 2 : %#x\n", get_new_p_val(vm, pro, p_code[0], p_val[0]), get_new_p_val(vm, pro, p_code[1], p_val[1]));
 				ft_printf("p_val[1] : %#X\n", p_val[1]);
 			}
-			pro->carry = 0;
+*/			pro->carry = 0;
 		}
 
 	}
