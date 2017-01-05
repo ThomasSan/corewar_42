fbaf2a62dfce68fd14204d64098dadc2813ad0d5
Thomas SANZEY <tsanzey@e2r10p7.42.fr>
2017-01-05 15:02:59 +0100

    Les leaks sont partis dans ton cul

--- gnl.c
+++ gnl.c
@@ -6,7 +6,7 @@
 /*   By: tsanzey <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 11:03:56 by tsanzey           #+#    #+#             */
-/*   Updated: 2015/12/15 11:03:58 by tsanzey          ###   ########.fr       */
+/*   Updated: 2017/01/05 15:00:09 by tsanzey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,34 +72,6 @@ char	*del_line(char *s, int i)
 	if (s)
 		free(s);
 	return (tmp);
-}
-
-char	*ft_strjoin2(char *s1, char *s2)
-{
-	char			*str;
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	len;
-
-	i = 0;
-	j = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
-	str = (char *)malloc(sizeof(str) * len + 1);
-	if (!str)
-		return (NULL);
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	free(s1);
-	return (str);
 }
 
 int		get_line_len(int const fd, char **dst)
